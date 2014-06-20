f5f366c34572f84ae676b664715c6f79616ae0ec
Alfredo Pantoja <chololoy@gmail.com>
2014-06-20 18:15:12 -0500

    Add tests

--- spec/controllers/content_controller_spec.rb
+++ spec/controllers/content_controller_spec.rb
@@ -1,25 +1,31 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe ContentController, :type => :controller do
+describe ContentController do
+
+  before (:each) do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+    @user.add_role :silver # gives the user a role. tests pass regardless of role
+  end  
 
   describe "GET 'silver'" do
     it "returns http success" do
       get 'silver'
-      expect(response).to be_success
+      response.should @user.has_role?(:silver) ? be_success : redirect_to(root_url)
     end
   end
 
   describe "GET 'gold'" do
     it "returns http success" do
       get 'gold'
-      expect(response).to be_success
+      response.should @user.has_role?(:gold) ? be_success : redirect_to(root_url)
     end
   end
 
   describe "GET 'platinum'" do
     it "returns http success" do
       get 'platinum'
-      expect(response).to be_success
+      response.should @user.has_role?(:platinum) ? be_success : redirect_to(root_url)
     end
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@
 
 FactoryGirl.define do
   factory :user do
+    name 'Alfredo'
+    sequence(:email) { |n| "email#{n}@gmail.com"}
+    password 'changeme'
+    password_confirmation 'changeme'
+    # required if the Devise Confirmable module is used
+    # confirmed_at Time.now
   end
 end
